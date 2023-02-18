aecb6f5aeb3c61c37daf1e63af21b9fd62e3c7d9
Dants0 <gui.dg1@hotmail.com>
2023-02-18 19:44:30 -0300

    new features add on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,21 +6,26 @@ import './footer.css'
 const Footer = () => {
     return (
         <footer>
+            <div className="container">
             <ul>
                 <li>
                     <h2 id='title'>Dev by Dantas</h2>
                 </li>
-                <li>
-                    <a href="https://github.com/Dants0">
+            </ul>
+            <li>
+                    <a href="https://github.com/Dants0" target="_blank">
                         <GithubLogo size={28} color="#1d1d1b" weight="thin" />
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/in/guilherme-góes-8b72531b0">
+                    <a href="https://www.linkedin.com/in/guilherme-góes-8b72531b0" target='_blank'>
                     <LinkedinLogo size={28} color="#1d1d1b" weight="thin" />
                     </a>
                 </li>
-            </ul>
+                <li>
+                    <a href="https://ko-fi.com/nosensee" target='_blank'>Buy me a coffe ☕</a>
+                </li>
+            </div>
         </footer>
     )
 }
